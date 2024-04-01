3905e0eb5604d875a738870f6fce414949f02634
Ar1sAlan1s <Arisalanis468@gmail.com>
2024-04-01 14:46:40 -0600

    Actualizacion de la clase

--- Main.java
+++ Main.java
@@ -1,75 +1,33 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner leer=new Scanner(System.in);
-        int opcion;Personal Empleado;Personal Empleado2=new Personal();
-        Visitantes visitante;
-        Visitantes visitantes = new Visitantes(" ", " ", " ", " ", " ");
-        for (boolean band=false;band==false;){
+        Scanner leer = new Scanner(System.in);
+        String Password = "Hola";
+        int opcion;
+        Personal Empleado;
+        Personal Empleado2 = new Personal();
+        System.out.print("Ingrese la contraseña:");
+        String pass = leer.nextLine();
+        while (!Password.equals(pass)) {
+            System.out.println("Intente de nuevo");
+            pass = leer.nextLine();
+        }
+        for (boolean band = false; band == false; ) {
 
             System.out.println("Ingrese su opcion");
-            System.out.println("3. Añadir Visitante o aumentar sus visitas con su CURP.");
-            System.out.println("4. Mostrar a todos los visitantes registrados en el sistema.");
-            System.out.println("5. Mostrar a un solo visitante.");
-            opcion=leer.nextInt();
-            switch (opcion){
+            opcion = leer.nextInt();
+            switch (opcion) {
                 case 1:
-                    leer.nextLine();
-                    System.out.print("Ingrese nombre:");
-                    String name=leer.nextLine();
-                    System.out.print("\nIngrese apellido:");
-                    String apellido=leer.nextLine();
-                    System.out.print("\nIngrese Fecha de nacimiento con el formato Dia/Mes/Año:");
-                    String FechaDeNacimiento=leer.nextLine();
-                    System.out.print("\nIngrese RFC:");
-                    String RFC=leer.nextLine();
-                    System.out.print("\nIngrese salario:");
-                    double salario=leer.nextDouble();
-                    leer.nextLine();
-                    System.out.print("\nIngrese rol:");
-                    String rol=leer.nextLine();
-                    System.out.print("\nIngrese Fecha de ingreso con el formato Dia/Mes/Año:");
-                    String FechaDeIngreso=leer.nextLine();
-                    System.out.print("\nIngrese CURP:");
-                    String CURP=leer.nextLine();
-                    System.out.print("\nIngrese Horario:");
-                    String horario=leer.nextLine();
-                    Empleado=new Personal(name,apellido,FechaDeNacimiento,RFC,salario,rol,FechaDeIngreso,CURP,horario);
-                    Empleado2.RegistrarEmpleado(Empleado);
+                    Empleado2.RegistrarEmpleado();
                     break;
                 case 2:
                     Empleado2.MostrarEmpleados();
                     break;
-                case 3: 
-                leer.nextLine();
-                System.out.println("¿Es su primera visita?(Si o No): ");
-                String resp = leer.nextLine();
-                if(resp.equalsIgnoreCase("si")){
-                System.out.println("Ingresar nombre del Visitante: ");
-                String nombre = leer.nextLine();
-                System.out.println("Ingresar apellidos del Visitante:  ");
-                String apellidos = leer.nextLine();
-                System.out.println("Ingresa su fecha de nacimiento: ");      
-                String fechaNacimiento = leer.nextLine();
-                System.out.println("Ingresa su CURP: ");
-                String curp = leer.nextLine();
-                System.out.println("Ingresar Fecha de Registro: ");
-                String fechaRegistro = leer.nextLine();
-                visitante = new Visitantes(nombre, apellidos, fechaNacimiento, curp, fechaRegistro);
-                visitantes.añadirVisitante(visitante);
-                }
-                else{ 
-                    visitantes.aumentarVisitas();   
-                }
-                break;
-                case 4: 
-                    visitantes.mostrarVisitantes();
-                    break;
-                case 5: 
-                    visitantes.getDatosVisitante();
+
+                case 3:
+                    Empleado2.QuitarEmpleados();
                     break;
             }
         }
-
     }
 }
--- Personal.java
+++ Personal.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Personal {
     ArrayList<Personal> Personal =new ArrayList<>();
+    Scanner leer=new Scanner(System.in);
     private String name;
     private String apellido;
     private  String FechaDeNacimiento;
@@ -64,8 +66,29 @@ public class Personal {
         return horario;
     }
 
-    public void RegistrarEmpleado(Personal Empleado){
-        Personal.add(Empleado);
+    public void RegistrarEmpleado(){
+        System.out.print("Ingrese nombre:");
+        String name=leer.nextLine();
+        System.out.print("\nIngrese apellido:");
+        String apellido=leer.nextLine();
+        System.out.print("\nIngrese Fecha de nacimiento con el formato Dia/Mes/Año:");
+        String FechaDeNacimiento=leer.nextLine();
+        System.out.print("\nIngrese RFC:");
+        String RFC=leer.nextLine();
+        System.out.print("\nIngrese salario:");
+        double salario=leer.nextDouble();
+        leer.nextLine();
+        System.out.print("\nIngrese rol:");
+        String rol=leer.nextLine();
+        System.out.print("\nIngrese Fecha de ingreso con el formato Dia/Mes/Año:");
+        String FechaDeIngreso=leer.nextLine();
+        System.out.print("\nIngrese CURP:");
+        String CURP=leer.nextLine();
+        System.out.print("\nIngrese Horario:");
+        String horario=leer.nextLine();
+        Personal empleado =new Personal(name,apellido,FechaDeNacimiento,RFC,salario,rol,FechaDeIngreso,CURP,horario);
+        Personal.add(empleado);
+
     }
 
     public void MostrarEmpleados(){
@@ -73,7 +96,16 @@ public class Personal {
             System.out.printf("Su nombre;%s\nSu Apellido:%s\nSu Fecha de Nacimiento;%s\nSu RFC:%s\nSu salario:%.2f\nSu rol:%s\nSu Fecha de ingreso;%s\nSu CURP:%s\nSu Horario;%s\n############################\n",Personal.get(i).getName(),Personal.get(i).getApellido(),Personal.get(i).getFechaDeNacimiento(),Personal.get(i).getRFC(),Personal.get(i).getSalario(),Personal.get(i).getRol(),Personal.get(i).getFechaDeIngreso(),Personal.get(i).getCURP(),Personal.get(i).getHorario());
         }
     }
-
+    public void QuitarEmpleados(){
+        System.out.println("La lista de empleados es la siguiente");
+        for (int i = 0;i < Personal.size();i++){
+            System.out.printf("%d Su nombre:%s\n   Su Apellido:%s\n",i, Personal.get(i).getName(),Personal.get(i).getApellido());
+        }
+        System.out.println("Ingrese elindice del empleado a eliminar:");
+        int op=leer.nextInt();
+        Personal.remove(op);
+        System.out.println("\nSe elimino exitosamente el trabajador");
+    }
 
 
 
